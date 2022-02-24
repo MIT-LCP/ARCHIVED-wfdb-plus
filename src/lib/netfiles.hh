2aa3d2688ce537341a4012a4faad9fe89894fc3f
Chen Xie <is.chenx@gmail.com>
2022-02-23 21:54:56 -0800

    Rename struct netfile to Netfile

--- src/lib/netfiles.hh
+++ src/lib/netfiles.hh
@@ -3,7 +3,7 @@
 
 #include <curl/curl.h>
 
-struct netfile {
+struct Netfile {
   char *url;
   char *data;
   int mode;
@@ -38,12 +38,12 @@ struct Chunk {
 #endif
 #endif
 
-/* values for netfile 'err' field */
+/* values for Netfile 'err' field */
 #define NF_NO_ERR 0   /* no errors */
 #define NF_EOF_ERR 1  /* file pointer at EOF */
 #define NF_REAL_ERR 2 /* read request failed */
 
-/* values for netfile 'mode' field */
+/* values for Netfile 'mode' field */
 #define NF_CHUNK_MODE 0 /* http range requests supported */
 #define NF_FULL_MODE 1  /* http range requests not supported */
 
@@ -76,42 +76,42 @@ Chunk *www_get_url_range_chunk(const char *url, long startb, long len);
 // Get all data from a given url
 Chunk *www_get_url_chunk(const char *url);
 // Free data structures associated with an open netfile
-void nf_delete(netfile *nf);
+void nf_delete(Netfile *nf);
 
-// Associate a netfile with a url
-netfile *nf_new(const char *url);
+// Associate a Netfile with a url
+Netfile *nf_new(const char *url);
 // get a block of data from a netfile
-long nf_get_range(netfile *nf, long startb, long len, char *rbuf);
+long nf_get_range(Netfile *nf, long startb, long len, char *rbuf);
 // Emulates feof, for netfiles
-int nf_feof(netfile *nf);
-// TRUE if netfile pointer points to EOF
-int nf_eof(netfile *nf);
+int nf_feof(Netfile *nf);
+// TRUE if Netfile pointer points to EOF
+int nf_eof(Netfile *nf);
 // Emulate fopen, for netfiles; read-only access
-netfile *nf_fopen(const char *url, const char *mode);
+Netfile *nf_fopen(const char *url, const char *mode);
 // Emulates fclose, for netfiles
-int nf_fclose(netfile *nf);
+int nf_fclose(Netfile *nf);
 // Emulates fgetc, for netfiles
-int nf_fgetc(netfile *nf);
+int nf_fgetc(Netfile *nf);
 // Emulates fgets, for netfiles
-static char *nf_fgets(char *s, int size, netfile *nf);
+static char *nf_fgets(char *s, int size, Netfile *nf);
 // Emulates fread, for netfiles
-size_t nf_fread(void *ptr, size_t size, size_t nmemb, netfile *nf);
+size_t nf_fread(void *ptr, size_t size, size_t nmemb, Netfile *nf);
 // Emulates fseek, for netfiles
-int nf_fseek(netfile *nf, long offset, int whence);
+int nf_fseek(Netfile *nf, long offset, int whence);
 // Emulates ftell, for netfiles
-long nf_ftell(netfile *nf);
+long nf_ftell(Netfile *nf);
 // Emulates ferror, for netfiles
-int nf_ferror(netfile *nf);
+int nf_ferror(Netfile *nf);
 // Emulates clearerr, for netfiles
-void nf_clearerr(netfile *nf);
+void nf_clearerr(Netfile *nf);
 // Emulates fflush, for netfiles [stub]
-int nf_fflush(netfile *nf);
+int nf_fflush(Netfile *nf);
 // Emulates fwrite, for netfiles [stub]
-size_t nf_fwrite(const void *ptr, size_t size, size_t nmemb, netfile *nf);
+size_t nf_fwrite(const void *ptr, size_t size, size_t nmemb, Netfile *nf);
 // Emulates putc, for netfiles) [stub]
-int nf_putc(int c, netfile *nf);
+int nf_putc(int c, Netfile *nf);
 // emulates fprintf, for netfiles) [stub]
-int nf_vfprintf(netfile *nf, const char *format, va_list ap);
+int nf_vfprintf(Netfile *nf, const char *format, va_list ap);
 
 char *curl_get_ua_string();
 int curl_try(CURLcode err);
@@ -119,7 +119,7 @@ unsigned int www_time();
 size_t curl_null_write(void *ptr, size_t size, size_t nmemb, void *stream);
 Chunk *curl_chunk_new(long len);
 void curl_chunk_delete(Chunk *c);
-Chunk *nf_get_url_range_chunk(netfile *nf, long startb, long len);
+Chunk *nf_get_url_range_chunk(Netfile *nf, long startb, long len);
 size_t curl_chunk_header_write(void *ptr, size_t size, size_t nmemb,
                                void *stream);
 size_t curl_chunk_write(void *ptr, size_t size, size_t nmemb, void *stream);
